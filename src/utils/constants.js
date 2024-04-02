f8d3c0f88d19b27c04874d0c9d6d163d2b7d511c
poseidonsanket <sanketdadali05@gmail.com>
2024-04-02 20:10:18 +0530

    Api Key Changed

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,4 +1,4 @@
-export const GOOGLE_API_KEY = "AIzaSyCU-SACfm011h4lLPgVBcDgVTI2lV4QOQ4";
+export const GOOGLE_API_KEY = "AIzaSyDX_pClqSt-MnsExKRvHU9FLWF4J0J45qw";
 
 export const YOUTUBE_VIDEOS_API =
   "https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&chart=mostPopular&maxResults=50&regionCode=IN&key=" +
